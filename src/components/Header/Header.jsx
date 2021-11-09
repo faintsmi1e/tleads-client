dc30e434bb9a005f92ef9596fec5614a8e5f206d
pinosol <zonest_64@mail.ru>
2021-11-10 02:47:00 +0300

    added header and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import classes from './Header.module.css';
 import { TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   return (
     <div className={classes.Header}>
-      <div>
+      <h2 style={{marginLeft:'15px'}}>Тестовое задание</h2>
+      <div className={classes.Search}>
+        <SearchIcon color='secondary' size='medium' style={{marginRight:'5px', cursor:'pointer'}}/>
         <TextField
           id='input-with-sx'
           label='Поиск'
--- src/pages/LeadTable.jsx
+++ src/pages/LeadTable.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import LeadService from '../API/LeadService';
 import { useFetching } from '../hooks/useFetching';
-import {
-  CircularProgress,
-  TableContainer,
-  Paper,
-} from '@mui/material';
+import { CircularProgress, TableContainer, Paper } from '@mui/material';
 import LeadRow from '../components/LeadRow/LeadRow';
 import Tableheader from '../components/TableHeader/TableHeader';
-import classes from './LeadTable.module.css'
+import classes from './LeadTable.module.css';
 import Header from '../components/Header/Header';
 const LeadTable = () => {
   const [leads, setLeads] = useState([]);
@@ -26,37 +22,41 @@ const LeadTable = () => {
   }, []);
   return (
     <>
-    <Header></Header>
-    <TableContainer
-      sx={{
-        border: 1,
-        borderRadius: '0px 0 6px 6px',
-        display: 'flex',
-        flexDirection: 'column',
-        boxSizing:'border-box',
-        padding:'15px',
-        minWidth:'800px'
-      }}
-      component={Paper}
-    >
-      
-      <Tableheader/>
+      <Header></Header>
+      <TableContainer
+        sx={{
+          border: 1,
+          borderRadius: '0px 0 6px 6px',
+          display: 'flex',
+          flexDirection: 'column',
+          boxSizing: 'border-box',
+          padding: '15px',
+          minWidth: '800px',
+        }}
+        component={Paper}
+      >
+        <Tableheader />
 
-      {leadError && <h1>{leadError}</h1>}
-      {isLeadsLoading ? (
-        <div className={classes.Loader}>
-        <CircularProgress color="secondary"></CircularProgress>
-        </div>
-      ) : (
-        leads.map((lead, id) => {
-          return <LeadRow sx={{
-            display: 'flex',
-            width: '100%',
-            
-          }} key={id} row={lead}></LeadRow>;
-        })
-      )}
-    </TableContainer>
+        {leadError && <h1>{leadError}</h1>}
+        {isLeadsLoading ? (
+          <div className={classes.Loader}>
+            <CircularProgress color='secondary'></CircularProgress>
+          </div>
+        ) : (
+          leads.map((lead, id) => {
+            return (
+              <LeadRow
+                sx={{
+                  display: 'flex',
+                  width: '100%',
+                }}
+                key={id}
+                row={lead}
+              ></LeadRow>
+            );
+          })
+        )}
+      </TableContainer>
     </>
   );
 };
